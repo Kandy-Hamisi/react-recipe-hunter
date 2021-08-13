76ffb0c0f6119d536e64349a4f57c4feef871e03
Kandy-Hamisi <hkandy2000@gmail.com>
2021-08-13 23:12:37 +0300

    worked on the recipe API from edamam

--- src/App.js
+++ src/App.js
@@ -1,22 +1,64 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React, { useEffect, useState, useRef } from 'react';
 function App() {
+  const[ingredientList, updateIngredientList] = useState([]);
+  const[loading, setLoading] = useState(false);
+  const API_KEY = "0852649f54a277171918d8778f65a799";
+  const APP_ID = "d114db93";
+  const inputRef = useRef(null);
+
+  const search = () => {
+    // console.log("input ref", inputRef.current.value);
+    searchForRecipe(inputRef.current.value);
+  }
+
+  const searchForRecipe = (query) => {
+    setLoading(true);
+    let url = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${API_KEY}`;
+    fetch(url)
+      .then(response => {
+        return response.json();
+      })
+      .then(res => {
+        // console.log("final response", res);
+        console.log(res.hits);
+        updateIngredientList(res.hits);
+        setLoading(false);
+      })
+      .catch(err => {
+        console.log("error", err)
+        setLoading(false);
+      });
+  }
+  // everytime compnent is mounted useEffect is called
+  useEffect(()=>{
+    searchForRecipe('chicken');
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <div>
+        <input ref={inputRef} placeholder="Search for Recipe" />
+        <button onClick={search}>Search</button>
+      </div>
+      {loading && <p>Loading...</p>}
+        <div className="wrapper">
+          {ingredientList.map((item) =>{
+            return (
+              <div className="Ingredient">
+                <span>{item.recipe.label}</span>
+
+                <img src={item.recipe.image} alt=""/>
+                <div className="Steps">
+                  {item.recipe.ingredientLines.map((step)=>{
+                    return <p>{step}</p>;
+                  })}
+                </div>
+              </div>
+            );
+          })}
+        </div>
       </header>
     </div>
   );
